af93c4fae8a07d672897f38af4b846d6759e4d37
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-27 23:34:52 +0900

    declare the routes for vue application

---
+++ src/pages/routes.js
@@ -0,0 +1,26 @@
+import {createRouter, createWebHistory } from 'vue-router'
+import Homepage from '@/pages/home/Home.vue';
+import SignIn from '@/pages//sign-in/SignIn.vue';
+import Cart from '@/pages//cart/Cart.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Homepage
+    },
+
+    {
+      path: '/sign-in/',
+      component: SignIn
+    },
+
+    {
+      path: '/cart/',
+      component: Cart
+    },
+  ],
+}); 
+
+export default router;
