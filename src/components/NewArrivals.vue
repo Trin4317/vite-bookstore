306990b2e690ab62b6e7fab8580e4d8cf7c34370
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-28 13:16:32 +0900

    update NewArrivals template with CSS

--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -6,9 +6,33 @@ import {useCart} from '@/stores/cart.js'
 </script>
 
 <template>
-  <div class="mt-6">
-    <p v-for="(book, index) in newArrivals" :key="index" v-text="book.title"></p>
+  <div class="container mx-auto">
+  <h1 class="text-center text-purple-800 my-6 text-6xl">New Arrivals</h1>
+
+  <div class="grid grid-cols-2 mb-2">
+    <div class="columns-1" v-for="bookDetail in newArrivals">
+      <div class="border flex rounded mb-4 shadow-xl relative mx-2">
+        <div class="p-4 flex flex-col static">
+          <strong class="inline-block mb-2 text-lime-200 text-left">{{ bookDetail.category }}</strong>
+          <h3 class="text-amber-700 text-left text-lg">{{ bookDetail.title }}</h3>
+          <div class="mb-1 text-purple-200 text-left font-normal">{{ bookDetail.author }}</div>
+          <p class="text-gray-300 text-left">{{ bookDetail.description }}</p>
+
+          <div class="flex justify-between mt-6 mx-6">
+            <span class="text-gray-300 mt-3 w-5 h-5 text-lg font-semibold mx-5" v-text="`\$${bookDetail.price}`"></span>
+            <button @click="addToCart(bookDetail)" class="w-12 h-12" type="button"><img src="/icons/basket_icon.svg" alt="Add to basket"></button>
+          </div>
+
+        </div>
+        <div class="col-auto block bg-white/[.8] p-2.5">
+          <img class="h-64" :src="bookDetail.image" :alt="bookDetail.title + ' by ' + bookDetail.author" />
+        </div>
+      </div>
+    </div>
+
+
   </div>
+</div>
 </template>
 
 <script>
