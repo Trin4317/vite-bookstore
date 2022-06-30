73fde61ba275e1183907e1c72b2e2f53e5ff5ded
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-30 18:17:03 +0900

    move SignIn page to pages/user directory

--- src/pages/user/sign-in/SignIn.vue
+++ src/pages/user/sign-in/SignIn.vue
@@ -10,7 +10,7 @@ import TopNavbar from '@/components/TopNavbar.vue';
       <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
       <p class="mt-2 text-center text-sm text-gray-600">
         Or
-        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> create a free account </a>
+        <router-link class="font-medium text-indigo-600 hover:text-indigo-500" to="/sign-up"> create a free account </router-link>
       </p>
     </div>
     <form class="mt-8 space-y-6" action="#" method="POST">
