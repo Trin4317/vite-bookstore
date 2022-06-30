d38fd673dd2670a809ef885d1f66061351f7d431
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-30 20:58:46 +0900

    define APP_BASE_URL global variable in vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
   // set publicPath to repository name to route all static assets to proper path
   // which is https://<account>.github.io/<repository-name>/
   base: process.env.NODE_ENV === "production" ? "/vite-bookstore/" : "/",
+  define: {
+  	APP_BASE_URL: process.env.NODE_ENV === "production" ? JSON.stringify("/vite-bookstore/") : JSON.stringify("/"),
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
