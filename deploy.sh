61491cc9b5f1c7e996cb36c6a5de179e6a3e2849
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-30 21:12:59 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-set -e
 npm run build
+git push origin --delete gh-pages
 git add dist && git commit -m 'adding dist subtree'
 git subtree push --prefix dist origin gh-pages
+git reset --hard HEAD^1
