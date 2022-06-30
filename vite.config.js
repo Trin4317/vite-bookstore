40ff3ad66cb4160891e52b2f8f20e050cbb7a76d
Trin4317 <eg.artourbabaev@gmail.com>
2022-06-30 19:39:26 +0900

    update vite config for deploying to github page

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // set publicPath to repository name to route all static assets to proper path
+  // which is https://<account>.github.io/<repository-name>/
+  base: process.env.NODE_ENV === "production" ? "/vite-bookstore/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
