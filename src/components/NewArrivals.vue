ff486207774adc20f2b5e51c65ce2267a3a21774
Trin4317 <eg.artourbabaev@gmail.com>
2022-07-03 18:45:49 +0900

    update CSS for NewArrivals component

--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -13,13 +13,13 @@ import {useCart} from '@/stores/cart.js'
     <div class="columns-1" v-for="bookDetail in newArrivals">
       <div class="border flex rounded mb-4 shadow-xl relative mx-2">
         <div class="p-4 flex flex-col static">
-          <strong class="inline-block mb-2 text-lime-200 text-left">{{ bookDetail.category }}</strong>
+          <strong class="inline-block mb-2 text-lime-400 text-left">{{ bookDetail.category }}</strong>
           <h3 class="text-amber-700 text-left text-lg">{{ bookDetail.title }}</h3>
-          <div class="mb-1 text-purple-200 text-left font-normal">{{ bookDetail.author }}</div>
-          <p class="text-gray-300 text-left">{{ bookDetail.description }}</p>
+          <div class="mb-1 text-purple-400 text-left font-normal">{{ bookDetail.author }}</div>
+          <p class="text-gray-600 text-left">{{ bookDetail.description }}</p>
 
           <div class="flex justify-between mt-6 mx-6">
-            <span class="text-gray-300 mt-3 w-5 h-5 text-lg font-semibold mx-5" v-text="`\$${bookDetail.price}`"></span>
+            <span class="text-gray-400 mt-3 w-5 h-5 text-lg font-semibold mx-5" v-text="`\$${bookDetail.price}`"></span>
             <button @click="addToCart(bookDetail)" class="w-12 h-12" type="button"><img src="/icons/basket_icon.svg" alt="Add to basket"></button>
           </div>
 
