e9912ef9ff84e96bf2cc8d90700cdd34fcadd5f7
Trin4317 <eg.artourbabaev@gmail.com>
2022-07-03 20:00:53 +0900

    fix Footer not display properly when app height is bigger than window height

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="absolute inset-x-0 bottom-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+  <footer :class="{'absolute' : appHeight < windowHeight }" class="inset-x-0 bottom-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">Bookstore</a> with Vite and Tailwind CSS. All Rights Reserved.
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
@@ -19,7 +19,8 @@
 export default {
   data() {
     return {
-
+      appHeight: document.getElementById('app').clientHeight,
+      windowHeight: window.innerHeight,
     };
   },
   methods: {
